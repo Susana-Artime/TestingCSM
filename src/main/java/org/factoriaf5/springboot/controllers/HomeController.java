8ed3b0f4a62487e48166cd06c7bfc061ae82588d
Arancha Fernandez <aferfer1978@gmail.com>
2024-10-21 12:30:43 +0200

    home controller done2

---
+++ src/main/java/org/factoriaf5/springboot/controllers/HomeController.java
@@ -0,0 +1,14 @@
+package org.factoriaf5.springboot.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    
+    @GetMapping(path = "")
+    public String index() {
+        return "Hello SpringBoot";
+    }
+
+}
